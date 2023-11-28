153b66e97c4faefb201cd7a6adcd8b51f0a67874
Benjamin Young <byoung@digitalbazaar.com>
2023-11-28 09:35:57 -0500

    Add CCG implementations repo.

--- _data/implementations.js
+++ _data/implementations.js
@@ -1,4 +1,8 @@
 module.exports = async () => {
-    const {allImplementations} = await import('vc-test-suite-implementations');
-    return Object.fromEntries(allImplementations.entries());
+    const {allImplementations: vcwgImplementations} = await import('vc-test-suite-implementations');
+    const {allImplementations: ccgImplementations} = await import('vc-api-test-suite-implementations');
+    return {
+        ...Object.fromEntries(vcwgImplementations.entries()),
+        ...Object.fromEntries(ccgImplementations.entries())
+    };
 };
