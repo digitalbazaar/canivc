25786f077bf9873e00b377b3016d6e15abadda1f
Benjamin Young <byoung@digitalbazaar.com>
2023-11-30 17:23:33 -0500

    Add five additional test reports.

--- _data/results.js
+++ _data/results.js
@@ -3,7 +3,11 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 module.exports = async function() {
   const urls = [
     'https://w3c.github.io/vc-di-eddsa-test-suite/index.json',
-    'https://w3c.github.io/vc-di-ecdsa-test-suite/index.json'
+    'https://w3c.github.io/vc-di-ecdsa-test-suite/index.json',
+    'https://w3c.github.io/vc-di-ed25519signature2020-test-suite/index.json',
+    'https://w3c-ccg.github.io/did-key-test-suite/index.json',
+    'https://w3c-ccg.github.io/status-list-2021-test-suite/index.json',
+    'https://w3c-ccg.github.io/vc-api-issuer-test-suite/index.json'
   ];
 
   /* This returns a promise */
