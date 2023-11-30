04b81cb165d0c6dcf8dbb451b123c214e08f02ad
Benjamin Young <byoung@digitalbazaar.com>
2023-11-30 16:40:19 -0500

    Fix linting issues in implementations.js.

--- _data/implementations.js
+++ _data/implementations.js
@@ -1,8 +1,13 @@
 module.exports = async () => {
-    const {allImplementations: vcwgImplementations} = await import('vc-test-suite-implementations');
-    const {allImplementations: ccgImplementations} = await import('vc-api-test-suite-implementations');
-    return {
-        ...Object.fromEntries(vcwgImplementations.entries()),
-        ...Object.fromEntries(ccgImplementations.entries())
-    };
+  const {
+    allImplementations: vcwgImplementations
+  } = await import('vc-test-suite-implementations');
+  const {
+    allImplementations: ccgImplementations
+  } = await import('vc-api-test-suite-implementations');
+
+  return {
+    ...Object.fromEntries(vcwgImplementations.entries()),
+    ...Object.fromEntries(ccgImplementations.entries())
+  };
 };
