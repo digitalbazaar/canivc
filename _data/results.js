2b68e4e4af014f07648a9f174ca38320d78dcec5
Benjamin Young <byoung@digitalbazaar.com>
2023-11-30 17:18:30 -0500

    Fix linting issues.

--- _data/results.js
+++ _data/results.js
@@ -1,15 +1,15 @@
 const EleventyFetch = require('@11ty/eleventy-fetch');
 
 module.exports = async function() {
-  let urls = [
+  const urls = [
     'https://w3c.github.io/vc-di-eddsa-test-suite/index.json',
     'https://w3c.github.io/vc-di-ecdsa-test-suite/index.json'
   ];
 
   /* This returns a promise */
-  const promises = urls.map((url) => EleventyFetch(url, {
+  const promises = urls.map(url => EleventyFetch(url, {
     duration: '1w', // save for 1 week
-    type: 'json'    // we’ll parse JSON for you
+    type: 'json' // we’ll parse JSON for you
   }));
   const results = await Promise.all(promises);
   return results;
