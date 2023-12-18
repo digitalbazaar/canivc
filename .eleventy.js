8f020b72d9e28eeb6d269f7bdb823e1028e33af4
James Easter <jeaster@digitalbazaar.com>
2023-12-18 12:24:07 -0700

    Add placeholder for search component and logic.

--- .eleventy.js
+++ .eleventy.js
@@ -74,6 +74,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('**/*.jpg');
   eleventyConfig.addPassthroughCopy('**/*.png');
+  eleventyConfig.addPassthroughCopy("./_js");
 
   // Return your Object options:
   return {
