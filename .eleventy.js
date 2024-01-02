f521724e05662174a8bae967cc77bdd64b941d51
James Easter <jeaster@digitalbazaar.com>
2024-01-02 16:56:59 -0700

    Remove unused .eleventy configuration.

--- .eleventy.js
+++ .eleventy.js
@@ -81,7 +81,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('**/*.jpg');
   eleventyConfig.addPassthroughCopy('**/*.png');
-  eleventyConfig.addPassthroughCopy("./_js");
 
   // Run pagefind script after site has built
   eleventyConfig.on('eleventy.after', () => {
